7346e90faf2b90559a68aed3aaea0c1d8504ffec
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-28 09:30:39 -0300

    ✨ Feat: Ux, Input & button

---
+++ web/src/Components/Ux/Input/Input.tsx
@@ -0,0 +1,14 @@
+
+export default function Input({ type, name, value, onChange, placeholder }) {
+  return (
+    <input
+      placeholder={placeholder}
+      type={type}
+      name={name}
+      value={value}
+      onChange={onChange}
+      className="mt-1 block w-full rounded-md border p-1 "
+      required
+    />
+  )
+}
