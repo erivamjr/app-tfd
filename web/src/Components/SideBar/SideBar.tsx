8e6a9036abe6583b75641266e01de0c04ed7fd3f
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-28 16:40:24 -0300

    👌 style: Code Review Changes

--- web/src/Components/SideBar/SideBar.tsx
+++ web/src/Components/SideBar/SideBar.tsx
@@ -1,90 +1,60 @@
-import { IoMenu } from 'react-icons/io5'
-import { CiHome } from 'react-icons/ci'
-import { AiOutlineProduct } from 'react-icons/ai'
-import { CgClipboard } from 'react-icons/cg'
-import { IoIosClose } from 'react-icons/io'
-import Logo from '../Ux/Logo/Vector.png'
+import { IoMenu } from 'react-icons/io5';
+import { CiHome, CiMedicalCase } from 'react-icons/ci';
+import { IoIosClose } from 'react-icons/io';
+import Logo from '../Ux/Logo/Vector.png';
 
-import React, { useState } from 'react'
-import { Link } from 'react-router-dom'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { GoPeople } from 'react-icons/go';
+import { TbReportSearch } from 'react-icons/tb';
+import { RiCalendarScheduleLine } from 'react-icons/ri';
+import { GiBrain } from 'react-icons/gi';
+
 export default function SideBar() {
-  const [sideBar, setSideBar] = useState(false)
+  const [sideBar, setSideBar] = useState(false);
+  const [page, setPage] = useState('Home');
+
   function handleSideBar() {
-    if (sideBar) {
-      setSideBar(false)
-    } else {
-      setSideBar(true)
-    }
+    setSideBar(!sideBar);
   }
+
   return (
-    <div className={` ${sideBar ? '' : 'sm:w-60'} `}>
+    <div className={` ${sideBar ? 'w-16' : 'w-60'} h-screen flex flex-col bg-[#008BAD] text-white transition-width duration-300`}>
       <div
         onClick={handleSideBar}
-        className={` ${sideBar ? 'h-screen' : ''
-          } flex justify-end text-3xl cursor-pointer text-white p-3 bg-[#008BAD]`}
+        className={`flex justify-end text-3xl cursor-pointer p-3 bg-[#008BAD]`}
       >
         {sideBar ? <IoMenu /> : <IoIosClose />}
       </div>
-      {!sideBar ? <div className='flex justify-end items-end bg-[#008BAD]'>
-        <img className='bg-[#008BAD]' src={Logo} />
-      </div> : <></>}
-
-      <div
-        className={` ${sideBar != false && 'hidden'
-          } h-screen flex flex-col justify-between p-3 bg-[#008BAD] text-white`}
-      >
-        <ul className='mt-20'>
-          <li className=" flex gap-1 items-center p-2 cursor-pointer hover:text-slate-900 hover:border-b">
-            <span>
-              <CiHome />
-            </span>
-            <Link to={'/'}>
-              <span>Home</span>
-            </Link>
-          </li>
-          <li className="flex gap-1 items-center p-2 cursor-pointer hover:text-slate-900 hover:border-b">
-            <span>
-              <AiOutlineProduct />
-            </span>
-            <Link to={'/pacientes'}>
-              <span>Pacientes</span>
-            </Link>
-          </li>
-          <li className="flex gap-1 items-center p-2 cursor-pointer hover:text-slate-900 hover:border-b">
-            <span>
-              <AiOutlineProduct />
-            </span>
-            <Link to={'/solicitacao'}>
-              <span>Solicitação</span>
-            </Link>
-          </li>
-          <Link to={'/relatorios'}>
-            <li className="flex gap-1 items-center p-2 cursor-pointer hover:text-slate-900 hover:border-b">
-              <span>
-                <AiOutlineProduct />
-              </span>
-              <span>Relatórios</span>
-            </li>
-          </Link>
-          <Link to={'/especialidades'}>
-            <li className="flex gap-1 items-center p-2 cursor-pointer hover:text-slate-900 hover:border-b">
-              <span>
-                <CgClipboard />
-              </span>
-              <span>Especialidades</span>
-            </li>
-          </Link>
-          <Link to={'/usuarios'}>
-            <li className="flex gap-1 items-center p-2 cursor-pointer hover:text-slate-900 hover:border-b">
-              <span>
-                <CgClipboard />
-              </span>
-              <span>Usuários</span>
-            </li>
-          </Link>
-
+      {!sideBar && (
+        <div className='flex justify-end items-center bg-[#008BAD]'>
+          <img className='w-36 h-36' src={Logo} alt="Logo" />
+        </div>
+      )}
+      <div className={`flex flex-col justify-between p-3 ${sideBar ? 'items-center' : 'items-start'}`}>
+        <ul className='mt-20 w-full'>
+          <MenuItem to="/" icon={<CiHome />} label="Home" currentPage={page} setPage={setPage} sideBar={sideBar} />
+          <MenuItem to="/pacientes" icon={<GiBrain />} label="Pacientes" currentPage={page} setPage={setPage} sideBar={sideBar} />
+          <MenuItem to="/solicitacao" icon={<RiCalendarScheduleLine />} label="Solicitação" currentPage={page} setPage={setPage} sideBar={sideBar} />
+          <MenuItem to="/relatorios" icon={<TbReportSearch />} label="Relatórios" currentPage={page} setPage={setPage} sideBar={sideBar} />
+          <MenuItem to="/especialidades" icon={<CiMedicalCase />} label="Especialidades" currentPage={page} setPage={setPage} sideBar={sideBar} />
+          <MenuItem to="/usuarios" icon={<GoPeople />} label="Usuários" currentPage={page} setPage={setPage} sideBar={sideBar} />
         </ul>
       </div>
     </div>
-  )
+  );
+}
+
+function MenuItem({ to, icon, label, currentPage, setPage, sideBar }) {
+  const isActive = currentPage === label;
+
+  return (
+    <li
+      onClick={() => setPage(label)}
+      className={` flex gap-1 items-center p-2 cursor-pointer ${isActive ? 'w-full bg-white rounded text-[#006A85]' : 'hover:text-[#006A85]'} ${sideBar ? 'justify-center' : ''}`}
+    >
+      <span>{icon}</span>
+      {!sideBar && <Link to={to}><span>{label}</span></Link>}
+    </li>
+  );
 }
