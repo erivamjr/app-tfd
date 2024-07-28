9732e771d4b89c4eb9ace1a63e67e9622713489b
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-28 17:49:09 -0300

    ✨ feat: Ux AdminToolbar

---
+++ web/src/Components/Ux/AdminToolbar/AdminToolbar.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export default function AdminToolbar({ children }) {
+  return (
+    <div className="mt-5 rounded bg-[#FFF9B7] border border-[#FFF15C]">
+
+      {children}
+    </div>
+  );
+}
