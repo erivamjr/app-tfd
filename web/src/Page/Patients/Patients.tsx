15d27798192e163eb4fa5f1503027d5e0bd63589
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-07-28 22:06:13 -0300

    ✨ feat: add modal Patients

--- web/src/Page/Patients/Patients.tsx
+++ web/src/Page/Patients/Patients.tsx
@@ -4,11 +4,30 @@ import AdminToolbar from '../../Components/Ux/AdminToolbar/AdminToolbar';
 import Input from '../../Components/Ux/Input/Input';
 import { CiSearch } from 'react-icons/ci';
 import { RiUserAddLine } from 'react-icons/ri';
+import Modal from '../../Components/Ux/Modal/Modal';
+import { useState } from 'react';
 
 export default function Patients() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  function handleOpenModal() {
+    setIsModalOpen(true);
+  }
+  function handleCloseModal() {
+    setIsModalOpen(false);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefaul()
+  }
   return (
     <div>
       <AdminToolbar>
+        <Modal isOpen={isModalOpen}
+          onClose={handleCloseModal}
+          title="Adicionar novo paciente">
+          <form onSubmit={handleSubmit} className="space-y-4">
+          </form>
+        </Modal>
         <div className='flex p-3 '>
           <div className='w-full flex items-center gap-2'>
             <span className='w-full'>
@@ -18,7 +37,7 @@ export default function Patients() {
               <CiSearch />
             </span>
           </div>
-          <div className='ml-10 bg-[#FFF58A] p-2 rounded flex items-center gap-2 cursor-pointer '>
+          <div onClick={handleOpenModal} className='ml-10 bg-[#FFF58A] p-2 rounded flex items-center gap-2 cursor-pointer '>
             <span>
               <RiUserAddLine />
             </span>
