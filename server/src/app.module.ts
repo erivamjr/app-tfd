4fd954f1be1678ce8a6b55d8071d60fdb9085c62
José Erivam <erivamdev@gmail.com>
2024-07-30 21:11:59 -0300

    :sparkles: feat: add CRUD of user

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpecialtyModule } from './specialty/specialty.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [SpecialtyModule],
+  imports: [SpecialtyModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
