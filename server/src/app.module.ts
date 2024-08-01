5a77d3130642649b3e1d8a4f105a76de16e9254c
José Erivam <erivamdev@gmail.com>
2024-08-01 18:44:51 -0300

    :sparkles: feat: add CRUD of appoiments

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SpecialtyModule } from './specialty/specialty.module';
 import { UserModule } from './user/user.module';
+import { AppointmentsModule } from './appointments/appointments.module';
 
 @Module({
-  imports: [SpecialtyModule, UserModule],
+  imports: [SpecialtyModule, UserModule, AppointmentsModule],
   controllers: [AppController],
   providers: [AppService],
 })
