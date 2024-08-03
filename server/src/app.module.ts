37bc536e823c9268d99e29cbb404bddadddbc43d
José Erivam <erivamdev@gmail.com>
2024-08-03 15:45:51 -0300

    :sparkles: feat: add function of forget and reset

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -8,6 +8,8 @@ import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { APP_GUARD } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { PatientsModule } from './patients/patients.module';
+import { MailerModule } from '@nestjs-modules/mailer';
+import { PugAdapter } from '@nestjs-modules/mailer/dist/adapters/pug.adapter';
 
 @Module({
   imports: [
@@ -23,6 +25,26 @@ import { PatientsModule } from './patients/patients.module';
     AppointmentsModule,
     forwardRef(() => AuthModule),
     PatientsModule,
+    MailerModule.forRoot({
+      transport: {
+        host: 'smtp.ethereal.email',
+        port: 587,
+        auth: {
+          user: 'jadon.bogisich@ethereal.email',
+          pass: 'nvdWh14V6HnvsnRPhh',
+        },
+      },
+      defaults: {
+        from: '"Tratamento Fora Domicilio" <erivamdev@gmail.com>',
+      },
+      template: {
+        dir: __dirname + '/templates',
+        adapter: new PugAdapter(),
+        options: {
+          strict: true,
+        },
+      },
+    }),
   ],
   controllers: [AppController],
   providers: [
--- server/src/auth/auth.service.ts
+++ server/src/auth/auth.service.ts
@@ -9,6 +9,7 @@ import { RegisterAuthDto } from './dto/register-auth.dto';
 import { UserService } from '../user/user.service';
 import { User } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
+import { MailerService } from '@nestjs-modules/mailer';
 
 @Injectable()
 export class AuthService {
@@ -19,6 +20,7 @@ export class AuthService {
     private readonly jwtService: JwtService,
     private readonly prisma: PrismaService,
     private readonly userService: UserService,
+    private readonly mailerService: MailerService,
   ) {}
 
   generateToken(user: User) {
@@ -83,7 +85,7 @@ export class AuthService {
   }
 
   async forget(email: string) {
-    const user = this.prisma.user.findUnique({
+    const user = await this.prisma.user.findUnique({
       where: {
         email,
       },
@@ -93,26 +95,53 @@ export class AuthService {
       throw new UnauthorizedException('Invalid credentials');
     }
 
-    // TO DO send email
+    const token = this.jwtService.sign(
+      {
+        id: user.id,
+      },
+      {
+        expiresIn: '15 minutes',
+        subject: String(user.id),
+        issuer: 'forget',
+        audience: this.audience,
+      },
+    );
+
+    await this.mailerService.sendMail({
+      to: email,
+      subject: 'Reset password',
+      template: 'forget',
+      context: {
+        name: user.name,
+        token,
+      },
+    });
 
     return true;
   }
 
   async reset(password: string, token: string) {
     //TO DO if token is valid
+    try {
+      const data = this.jwtService.verify(token, {
+        issuer: 'forget',
+        audience: this.audience,
+      });
+      const id = data.id; // TO DO get id from token
 
-    const id = 'fasdfa'; // TO DO get id from token
+      const user = await this.prisma.user.update({
+        where: {
+          id,
+        },
+        data: {
+          password,
+        },
+      });
 
-    const user = await this.prisma.user.update({
-      where: {
-        id,
-      },
-      data: {
-        password,
-      },
-    });
-
-    return this.generateToken(user);
+      return this.generateToken(user);
+    } catch (e) {
+      throw new BadRequestException('Invalid token');
+    }
   }
 
   async register(data: RegisterAuthDto) {
