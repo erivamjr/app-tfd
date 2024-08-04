d9ba625d1009a8a7fb8a815dcf1cbd5b92d89dc6
José Erivam <erivamdev@gmail.com>
2024-08-04 20:32:37 -0300

    :sparkles: feat: add dto create patients

--- server/src/appointments/dto/create-appointment.dto.ts
+++ server/src/appointments/dto/create-appointment.dto.ts
@@ -1,1 +1,50 @@
-export class CreateAppointmentDto {}
+import { Priority } from '@prisma/client';
+import { IsEnum, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateAppointmentDto {
+  @IsNumber()
+  @IsNotEmpty()
+  specialtyId: number;
+
+  @IsNotEmpty()
+  @IsString()
+  patientId: string;
+
+  @IsNotEmpty()
+  @IsString()
+  userId: string;
+
+  @IsEnum(Priority)
+  @IsNotEmpty()
+  priority: Priority;
+
+  @IsNotEmpty()
+  @IsString()
+  appointmentDate: string;
+
+  @IsString()
+  diagnosis?: string;
+
+  @IsString()
+  cid?: string;
+
+  @IsString()
+  requestingDoctor?: string;
+
+  @IsString()
+  crm?: string;
+
+  @IsString()
+  requestCode?: string;
+
+  @IsNotEmpty()
+  @IsString()
+  requestDate: string;
+
+  @IsNotEmpty()
+  @IsString()
+  status: string;
+
+  @IsString()
+  notes?: string;
+}
