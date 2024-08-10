264a18752e23b2dfac5ebc9ff2cf0f39c216ea71
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-10 11:02:00 -0300

    ✨Feat: Hooks api usePatients

---
+++ web/src/Components/Hooks/Api/Patiens/Patiens.ts
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+import { Patient } from './types'
+import api from '../../../../Api'
+
+const usePatients = () => {
+  const [patients, setPatients] = useState<Patient[]>([])
+  const [isLoading, setIsLoading] = useState<boolean>(false)
+  const [isError, setIsError] = useState<boolean>(false)
+
+  useEffect(() => {
+    const controller = new AbortController()
+    const { signal } = controller
+
+    const fetchPatients = async () => {
+      try {
+        setIsLoading(true)
+        const response = await api.get<{ data: Patient[] }>('/patients', { signal })
+        console.log('Data received:', response.data.data)
+        setPatients(response.data.data)
+      } catch (error) {
+        if (axios.isAxiosError(error) && error.message === 'canceled') {
+          console.log('Request cancelled')
+        } else {
+          console.error('Error fetching data:', error)
+          setIsError(true)
+        }
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    fetchPatients()
+
+    return () => {
+      controller.abort()
+    }
+  }, [])
+
+  return { patients, isLoading, isError }
+}
+
+export default usePatients
---
+++ web/src/Components/Hooks/Api/Patiens/TypePatiens.ts
@@ -0,0 +1,11 @@
+export interface Patient {
+  id: string
+  name: string
+  cpf: string
+  rg: string
+  susCard: string
+  phone: string
+  motherName: string
+  address: string
+  city: string
+}
