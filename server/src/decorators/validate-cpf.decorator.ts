d071b7e456a3e4163208a2aa610ae9b2558050d5
José Erivam <erivamdev@gmail.com>
2024-08-11 15:02:10 -0300

    :sparkles: feat: add function of the validate cpf and params validator cpf

---
+++ server/src/decorators/validate-cpf.decorator.ts
@@ -0,0 +1,63 @@
+import {
+  registerDecorator,
+  ValidationOptions,
+  ValidatorConstraint,
+  ValidatorConstraintInterface,
+} from 'class-validator';
+
+@ValidatorConstraint({ async: false })
+export class IsCpfConstraint implements ValidatorConstraintInterface {
+  validate(cpf: string) {
+    cpf = cpf.replace(/[^\d]+/g, '');
+    if (cpf.length !== 11) return false;
+    if (
+      [
+        '00000000000',
+        '11111111111',
+        '22222222222',
+        '33333333333',
+        '44444444444',
+        '55555555555',
+        '66666666666',
+        '77777777777',
+        '88888888888',
+        '99999999999',
+      ].includes(cpf)
+    )
+      return false;
+
+    let sum = 0;
+    let remainder;
+
+    for (let i = 1; i <= 9; i++)
+      sum = sum + parseInt(cpf.substring(i - 1, i)) * (11 - i);
+    remainder = (sum * 10) % 11;
+    if (remainder === 10 || remainder === 11) remainder = 0;
+    if (remainder !== parseInt(cpf.substring(9, 10))) return false;
+
+    sum = 0;
+    for (let i = 1; i <= 10; i++)
+      sum = sum + parseInt(cpf.substring(i - 1, i)) * (12 - i);
+    remainder = (sum * 10) % 11;
+    if (remainder === 10 || remainder === 11) remainder = 0;
+    if (remainder !== parseInt(cpf.substring(10, 11))) return false;
+
+    return true;
+  }
+
+  defaultMessage() {
+    return 'CPF invalid';
+  }
+}
+
+export function IsCpf(validationOptions?: ValidationOptions) {
+  return function (object: object, propertyName: string) {
+    registerDecorator({
+      target: object.constructor,
+      propertyName: propertyName,
+      options: validationOptions,
+      constraints: [],
+      validator: IsCpfConstraint,
+    });
+  };
+}
