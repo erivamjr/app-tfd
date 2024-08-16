93fca10e02305a94c08666424a202097c303bee3
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-08-15 21:54:32 -0300

    💄Feat:  style & interface

--- web/src/Components/Ux/Table/TableActions.tsx
+++ web/src/Components/Ux/Table/TableActions.tsx
@@ -1,9 +1,24 @@
 import { Link } from 'react-router-dom'
 
-export const TableActions = ({ id, icon, url, color, text }) => (
+interface TableActionsProps {
+  id: string
+  icon: React.ReactNode
+  url: string
+  color: string
+  text: string
+}
+
+export const TableActions = ({
+  id,
+  icon,
+  url,
+  color,
+  text,
+}: TableActionsProps) => (
   <Link to={`/${url}/${id}`}>
     <div
-      className={` flex gap-3 items-center justify-center text-2xl bg-${color}-600 hover:bg-${color}-500 text-${text} rounded p-3`}
+      style={{ background: color, color: text }}
+      className={` flex gap-3 items-center justify-center text-2xl rounded p-3 hover:opacity-80`}
     >
       {icon}
     </div>
