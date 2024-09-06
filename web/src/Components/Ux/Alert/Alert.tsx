5751f6124a496837e0960340c1b1fdcf535dc5d6
Jose Erisvaldo <joseerisvaldopg@gmail.com>
2024-09-05 21:27:54 -0300

    Feat: sending scheduling data to api

--- web/src/Components/Ux/Alert/Alert.tsx
+++ web/src/Components/Ux/Alert/Alert.tsx
@@ -1,8 +1,20 @@
 import './style.css'
-export default function Alert({ color, message, text }) {
+
+export default function Alert({ type, message }) {
+  // Define as classes de acordo com o tipo de alerta
+  const alertClasses = {
+    success: 'bg-green-500 text-white',
+    error: 'bg-red-500 text-white',
+    warning: 'bg-yellow-500 text-black',
+    info: 'bg-blue-500 text-white',
+  }
+
+  // Seleciona a classe baseada no tipo
+  const alertClass = alertClasses[type] || 'bg-gray-500 text-white' // Classe padrão se o tipo for inválido
+
   return (
     <div
-      className={`animate bg-${color}-600 text-${text} absolute top-32 right-10  p-2 rounded-lg shadow-lg`}
+      className={` z-50 animate ${alertClass} absolute top-32 right-10 p-2 rounded-lg shadow-lg`}
     >
       {message}
     </div>
