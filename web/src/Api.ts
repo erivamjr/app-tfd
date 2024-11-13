24e4b3dbccd7a2f5a941ed8a9b0de3afa7716ebc
José Erivam <erivamdev@gmail.com>
2024-11-13 17:51:20 -0300

    :bug: fix: change address api

--- web/src/Api.ts
+++ web/src/Api.ts
@@ -1,8 +1,9 @@
 import axios from 'axios'
 const api = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: 'https://app-tfd.onrender.com/',
 })
 
+// 'http://localhost:3000'
 // 'https://app-tfd.onrender.com/'
 
 export default api
