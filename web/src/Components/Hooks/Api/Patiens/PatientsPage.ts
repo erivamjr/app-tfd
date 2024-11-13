e3be0042b29e0c53deb7834eb141030badb55ac5
José Erivam <erivamdev@gmail.com>
2024-11-13 17:45:10 -0300

    :bug: fix: change type search get by api

--- web/src/Components/Hooks/Api/Patiens/PatientsPage.ts
+++ web/src/Components/Hooks/Api/Patiens/PatientsPage.ts
@@ -3,7 +3,11 @@ import axios from 'axios'
 import api from '../../../../Api'
 import { Patient } from './TypePatiens'
 
-const usePatientsPage = (currentPage: number, itemsPerPage: number) => {
+const usePatientsPage = (
+  currentPage: number,
+  itemsPerPage: number,
+  searchTerm: string,
+) => {
   const [patientsPage, setPatientsPage] = useState<Patient[]>([])
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [isError, setIsError] = useState<boolean>(false)
@@ -14,25 +18,44 @@ const usePatientsPage = (currentPage: number, itemsPerPage: number) => {
     const { signal } = controller
 
     const fetchPatientsPage = async () => {
-      try {
-        setIsLoading(true)
-        const response = await api.get<{
-          data: Patient[]
-          total: number
-          pageCount: number
-        }>(`/patients?page=${currentPage}&limit=${itemsPerPage}`, { signal })
+      if (searchTerm) {
+        try {
+          setIsLoading(true)
+          setIsError(false)
 
-        setPatientsPage(response.data.data)
-        setTotalPages(response.data.pageCount)
-      } catch (error) {
-        if (axios.isAxiosError(error) && error.message === 'canceled') {
-          console.log('Request was canceled.')
-        } else {
-          console.error('Error fetching data:', error)
+          const response = await api.get<Patient[]>(
+            `/patients/search?name=${searchTerm}`,
+            { signal },
+          )
+          console.log('TESTANDO USEPACIENTES', response.data)
+
+          setPatientsPage(response.data)
+        } catch (error) {
           setIsError(true)
+        } finally {
+          setIsLoading(false)
         }
-      } finally {
-        setIsLoading(false)
+      } else {
+        try {
+          setIsLoading(true)
+          const response = await api.get<{
+            data: Patient[]
+            total: number
+            pageCount: number
+          }>(`/patients?page=${currentPage}&limit=${itemsPerPage}`, { signal })
+
+          setPatientsPage(response.data.data)
+          setTotalPages(response.data.pageCount)
+        } catch (error) {
+          if (axios.isAxiosError(error) && error.message === 'canceled') {
+            console.log('Request was canceled.')
+          } else {
+            console.error('Error fetching data:', error)
+            setIsError(true)
+          }
+        } finally {
+          setIsLoading(false)
+        }
       }
     }
 
@@ -41,7 +64,7 @@ const usePatientsPage = (currentPage: number, itemsPerPage: number) => {
     return () => {
       controller.abort()
     }
-  }, [currentPage, itemsPerPage])
+  }, [currentPage, itemsPerPage, searchTerm])
 
   return { patientsPage, isLoading, isError, totalPages }
 }
