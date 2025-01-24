d98da89ee9a151020d55d2f23f69678e65f54521
José Erivam <erivamdev@gmail.com>
2025-01-24 20:13:26 -0300

    :recycle: refactor: styles in headers profile

---
+++ web/src/Components/Ux/Card/index.tsx
@@ -0,0 +1,22 @@
+interface CardProps {
+  title: string
+  icon: React.ReactNode
+  number: number
+  color: string
+}
+
+export const Card = ({ title, icon, number, color }: CardProps) => {
+  return (
+    <div
+      className={`bg-${color}-100 text-gray-700 p-6 rounded-lg shadow-md  max-w-xs`}
+    >
+      <div className="flex justify-between items-center">
+        <h3 className="text-xl font-semibold">{title}</h3>
+        <div className={`bg-${color}-500 rounded-full p-3 text-white`}>
+          {icon}
+        </div>
+      </div>
+      <p className="text-3xl font-bold">{number}</p>
+    </div>
+  )
+}
