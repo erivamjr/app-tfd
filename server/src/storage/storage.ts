e9bcedd31cd7bb36b8fd8127ccda79dc09d86c13
José Erivam <erivamdev@gmail.com>
2025-01-28 19:03:48 -0300

    :sparkles: feat: add interface get signed url

--- server/src/storage/storage.ts
+++ server/src/storage/storage.ts
@@ -2,4 +2,5 @@ import { FileDto } from '../auth/dto/file.dto';
 
 export abstract class IStorage {
   abstract upload(file: FileDto, folder: string): Promise<string>;
+  abstract getSignedUrl(filePath: string): Promise<string>;
 }
