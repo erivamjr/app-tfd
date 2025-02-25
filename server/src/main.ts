d0b1bd9ad8f5577287cb8bb2cc41dfd835e639d5
José Erivam <erivamdev@gmail.com>
2025-02-25 15:56:47 -0300

    :sparkles: feat: add security from the server to the front end only.

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,11 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
-  app.enableCors();
+  app.enableCors({
+    origin: configService.get<string>('FRONTEND_URL'),
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
